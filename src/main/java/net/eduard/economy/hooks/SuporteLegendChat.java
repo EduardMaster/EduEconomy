476d0737d46caf92623f00d484fa83c987557ab4
Eduard Dev <eduardkiller@hotmail.com>
2021-05-02 21:41:38 -0300

    Juntando sistema EduMagnata para dentro do Economy

---
+++ src/main/java/net/eduard/economy/hooks/SuporteLegendChat.java
@@ -0,0 +1,19 @@
+package net.eduard.economy.hooks;
+
+import net.eduard.api.lib.manager.EventsManager;
+
+public class SuporteLegendChat extends EventsManager {
+
+
+	/*
+	@EventHandler
+	public void chat(ChatMessageEvent e) {
+		Player p = e.getSender();
+		if (p.getName().equalsIgnoreCase(MagnataGerador.getPlayerMagnata())) {
+			e.setTagValue("magnata", Main.getInstance().getConfigs().message("tag-magnata"));
+		}
+
+	}
+
+	 */
+}
